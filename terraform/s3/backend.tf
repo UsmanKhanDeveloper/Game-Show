0132461221960deb60bfd1e4e26c222890418007
UsmanKhanDeveloper <159197368+UsmanKhanDeveloper@users.noreply.github.com>
2025-09-18 18:18:42 -0400

    Update Terraform organization and workspace names

--- terraform/s3/backend.tf
+++ terraform/s3/backend.tf
@@ -1,10 +1,10 @@
 terraform { 
   cloud { 
     
-    organization = "sanskrit-gameshow" 
+    organization = "SanskritDeploymentTF" 
 
     workspaces { 
-      name = "gameshow-s3" 
+      name = "s3" 
     } 
   } 
 }
