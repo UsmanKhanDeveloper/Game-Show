840a6f043d4376d2caa261780a47f396e89bcac0
UsmanKhanDeveloper <159197368+UsmanKhanDeveloper@users.noreply.github.com>
2025-09-18 18:20:16 -0400

    Update Terraform Cloud organization and workspace names

--- terraform/cloudFront/backend.tf
+++ terraform/cloudFront/backend.tf
@@ -1,10 +1,10 @@
 terraform { 
   cloud { 
     
-    organization = "sanskrit-gameshow" 
+    organization = "SanskritDeploymentTF" 
 
     workspaces { 
-      name = "gameshow-cloudFront" 
+      name = "cloud-front" 
     } 
   } 
 }
